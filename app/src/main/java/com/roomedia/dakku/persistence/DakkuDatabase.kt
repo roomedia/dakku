c98454211080133c4cca944ced2f27f51b9ef7e1
김동호 <7322527+roomedia@users.noreply.github.com>
2021-04-01 11:59:50 +0900

    데이터베이스 초기 입력 데이터 Primary Key 수정 (#22)
    * 데이터베이스 이름 수정
    
    * 데이터베이스 초기 입력 데이터 Primary Key Unique 하게 변경

--- app/src/main/java/com/roomedia/dakku/persistence/DakkuDatabase.kt
+++ app/src/main/java/com/roomedia/dakku/persistence/DakkuDatabase.kt
@@ -19,7 +19,7 @@ abstract class DakkuDatabase : RoomDatabase() {
         val instance: DakkuDatabase = Room.databaseBuilder(
             DakkuApplication.instance,
             DakkuDatabase::class.java,
-            "contact-database"
+            "dakku-database"
         )
             .addCallback(setInitialRoomDatabaseCallback())
             .build()
@@ -34,19 +34,19 @@ abstract class DakkuDatabase : RoomDatabase() {
                         deleteAll()
                         insert(
                             Diary(simpleDateFormat.parse("2021-02-10")!!.time, "210210"),
-                            Diary(simpleDateFormat.parse("2021-02-10")!!.time, "210210-2ㅜㅜ", lock = true),
+                            Diary(simpleDateFormat.parse("2021-02-10")!!.time + 1, "210210-2ㅜㅜ", lock = true),
                             Diary(simpleDateFormat.parse("2021-02-13")!!.time, "210213 오늘의 일", bookmark = true),
                             Diary(simpleDateFormat.parse("2021-02-14")!!.time, ".", lock = true),
                             Diary(simpleDateFormat.parse("2021-02-15")!!.time, "배고프다", bookmark = true),
                             Diary(simpleDateFormat.parse("2021-02-18")!!.time, "210218"),
                             Diary(simpleDateFormat.parse("2021-02-20")!!.time, "210220"),
                             Diary(simpleDateFormat.parse("2021-02-21")!!.time, "210221"),
-                            Diary(simpleDateFormat.parse("2021-02-21")!!.time, "210227"),
+                            Diary(simpleDateFormat.parse("2021-02-21")!!.time + 1, "210227"),
                             Diary(simpleDateFormat.parse("2021-02-27")!!.time, "뭐더라", lock = true),
                             Diary(simpleDateFormat.parse("2021-03-01")!!.time, "오늘 어이 없었던 거..,,", bookmark = true, lock = true),
                             Diary(simpleDateFormat.parse("2021-03-02")!!.time, "210302"),
                             Diary(simpleDateFormat.parse("2021-03-04")!!.time, "210304"),
-                            Diary(simpleDateFormat.parse("2021-03-04")!!.time, "?????"),
+                            Diary(simpleDateFormat.parse("2021-03-04")!!.time + 1, "?????"),
                             Diary(simpleDateFormat.parse("2021-03-05")!!.time, "아진짜"),
                             Diary(simpleDateFormat.parse("2021-03-06")!!.time, "배고파.."),
                         )
