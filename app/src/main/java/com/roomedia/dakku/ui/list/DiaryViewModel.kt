d2dc8f366d0b4d9d45a80e3f9872b6c0d95ac6a9
roomedia <roomedia@naver.com>
2021-04-05 22:06:05 +0900

    다이어리 썸네일 로드

--- app/src/main/java/com/roomedia/dakku/ui/list/DiaryViewModel.kt
+++ app/src/main/java/com/roomedia/dakku/ui/list/DiaryViewModel.kt
@@ -2,7 +2,9 @@ package com.roomedia.dakku.ui.list
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.core.content.ContextCompat.startActivity
+import androidx.core.graphics.drawable.toDrawable
 import androidx.databinding.ObservableBoolean
 import com.roomedia.dakku.persistence.Diary
 import com.roomedia.dakku.repository.DiaryRepository
@@ -12,11 +14,13 @@ import com.roomedia.dakku.ui.util.REQUEST
 import com.roomedia.dakku.ui.util.showPasswordOpenDialog
 import com.roomedia.dakku.ui.util.showPasswordUnlockDialog
 
-class DiaryViewModel(private val diary: Diary) : CommonViewModel<Diary>() {
+class DiaryViewModel(private val diary: Diary, context: Context) : CommonViewModel<Diary>() {
     override val repository = DiaryRepository()
     val isBookmarked = ObservableBoolean(diary.bookmark)
     val isLocked = ObservableBoolean(diary.lock)
+
     val title = diary.title
+    val thumbnail = BitmapFactory.decodeFile("${context.filesDir.path}/${diary.id}")?.toDrawable(context.resources)
 
     fun onThumbnail(context: Context) {
         val intent = Intent(context, DiaryEditorActivity::class.java).apply {
