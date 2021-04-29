74e9606aacd9636944b98541c1bfa6e39fc8e1b5
김동호 <7322527+roomedia@users.noreply.github.com>
2021-04-29 21:04:16 +0900

    Release를 위한 필수 기능만 포함하여 출시 (#39)
    * 아이콘 추가
    
    * 미구현 기능, 메뉴 버튼 UI 비활성화
    
    * 초기 테스트 데이터 삭제

--- app/src/main/java/com/roomedia/dakku/persistence/DakkuDatabase.kt
+++ app/src/main/java/com/roomedia/dakku/persistence/DakkuDatabase.kt
@@ -4,12 +4,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.roomedia.dakku.DakkuApplication
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 @TypeConverters(DakkuTypeConverter::class)
 @Database(entities = [Diary::class, Sticker::class], version = 1, exportSchema = false)
@@ -22,39 +17,6 @@ abstract class DakkuDatabase : RoomDatabase() {
             DakkuApplication.instance,
             DakkuDatabase::class.java,
             "dakku-database"
-        )
-            .addCallback(setInitialRoomDatabaseCallback())
-            .build()
-
-        // MARK: test input data
-        private fun setInitialRoomDatabaseCallback() = object : RoomDatabase.Callback() {
-            override fun onCreate(db: SupportSQLiteDatabase) {
-                super.onCreate(db)
-                val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-                GlobalScope.launch {
-                    instance.diaryDao().apply {
-                        deleteAll()
-                        insert(
-                            Diary(simpleDateFormat.parse("2021-02-10")!!.time, "210210"),
-                            Diary(simpleDateFormat.parse("2021-02-10")!!.time + 1, "210210-2ㅜㅜ", lock = true),
-                            Diary(simpleDateFormat.parse("2021-02-13")!!.time, "210213 오늘의 일", bookmark = true),
-                            Diary(simpleDateFormat.parse("2021-02-14")!!.time, ".", lock = true),
-                            Diary(simpleDateFormat.parse("2021-02-15")!!.time, "배고프다", bookmark = true),
-                            Diary(simpleDateFormat.parse("2021-02-18")!!.time, "210218"),
-                            Diary(simpleDateFormat.parse("2021-02-20")!!.time, "210220"),
-                            Diary(simpleDateFormat.parse("2021-02-21")!!.time, "210221"),
-                            Diary(simpleDateFormat.parse("2021-02-21")!!.time + 1, "210227"),
-                            Diary(simpleDateFormat.parse("2021-02-27")!!.time, "뭐더라", lock = true),
-                            Diary(simpleDateFormat.parse("2021-03-01")!!.time, "오늘 어이 없었던 거..,,", bookmark = true, lock = true),
-                            Diary(simpleDateFormat.parse("2021-03-02")!!.time, "210302"),
-                            Diary(simpleDateFormat.parse("2021-03-04")!!.time, "210304"),
-                            Diary(simpleDateFormat.parse("2021-03-04")!!.time + 1, "?????"),
-                            Diary(simpleDateFormat.parse("2021-03-05")!!.time, "아진짜"),
-                            Diary(simpleDateFormat.parse("2021-03-06")!!.time, "배고파.."),
-                        )
-                    }
-                }
-            }
-        }
+        ).build()
     }
 }
